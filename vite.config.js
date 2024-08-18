8c40d7f075aed4674a5519f50e68e1c1794175ce
Marta Smuk <msmuk999@gmail.com>
2024-08-18 19:36:11 +0200

    test1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'My_notes_list_hw_37'
+  base: 'module_styles_hw_37'
 })
