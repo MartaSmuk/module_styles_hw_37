700722fda4b0f2a221f7ee432487243b282dd6f3
Marta Smuk <msmuk999@gmail.com>
2024-08-18 19:19:30 +0200

    changed build base file name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'My_notes_list_hw_35'
+  base: 'My_notes_list_hw_37'
 })
